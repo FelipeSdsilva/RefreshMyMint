a8158d7b7d0f225b544d443b9fadada3b46bf83b
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-01 20:24:45 -0300

    Initialize exercice list, create class

---
+++ src/exercices/ListExercice.java
@@ -0,0 +1,17 @@
+package exercices;
+
+import java.util.Scanner;
+
+import views.MensagensObjects;
+import views.StringText;
+
+public class ListExercice {
+
+	Scanner in = new Scanner(System.in);
+	MensagensObjects ms = new MensagensObjects();
+	StringText msg = new StringText();
+	
+	public void exercice01() {
+		
+	}
+}
