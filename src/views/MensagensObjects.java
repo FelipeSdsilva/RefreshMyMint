38c678999e45b976a558bfc1eaf9a7fcfd344a2d
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-02 18:58:34 -0300

    Initialize exercice01 in class ListExercice

--- src/views/MensagensObjects.java
+++ src/views/MensagensObjects.java
@@ -2,63 +2,68 @@ package views;
 
 public class MensagensObjects {
 
-	public String[] account = { "Enter account number: ", // 0
-			"Enter account holder: ", // 1
-			"Is there na initial deposit (y/n)? ", // 2
-			"Enter initial deposit value:  ", // 3
-			"Account data:", // 4
-			"Enter deposit value:", // 5
-			"Update account:", // 6
-			"Enter a withdraw value: "// 7
-	};
+    public String[] account = {"Enter account number: ", // 0
+            "Enter account holder: ", // 1
+            "Is there na initial deposit (y/n)? ", // 2
+            "Enter initial deposit value:  ", // 3
+            "Account data:", // 4
+            "Enter deposit value:", // 5
+            "Update account:", // 6
+            "Enter a withdraw value: "// 7
+    };
 
-	public String[] employee = { "Value per hours: ", // 0
-			"Working hours: ", // 1
-			"The payment for ", // 2
-			"is ", // 3
-			"Which percentage to increase salary?", // 4
-			"New salary = R$ ", // 5
-			"Increasy = R$ ", // 6
-			"Percentege = ", // 7
-			"Write the salary of person: " };
+    public String[] employee = {"Value per hours: ", // 0
+            "Working hours: ", // 1
+            "The payment for ", // 2
+            "is ", // 3
+            "Which percentage to increase salary?", // 4
+            "New salary = R$ ", // 5
+            "Increasy = R$ ", // 6
+            "Percentege = ", // 7
+            "Write the salary of person: ",// 8
+            "How many employee will be register? ",// 9
+            "Employee #%d: \n",// 10
+            "Salary: ",// 11
+            "Id: ",// 12
+    };
 
-	public String[] person = { "How many persons will you type? ", // 0
-			"Datas of ", // 1
-			"a person: ", // 2
-			"Name: ", // 3
-			"Age: ", // 4
-			"Height: ", // 5
-			"Average height: ", // 6
-			"People whit less 16 year old: ", // 7
-			"Person: ", // 8
-			"A age avarege ", // 9
-			" is the ", // 10
-			" and ", // 11
-			" years old", // 12
-			" ", // 13
-			"Write ages: ", // 14
-			"How a many persons you go register? ", // 15
-			"Person more old: ", // 16
-			"Height of %da person: ", // 17
-			"Gender of %da person: ", // 18
-			"Number of mans = ", // 19
-			"Avarege height of womans = ", // 20
-			"Height smaller = ", // 21
-			"Height Bigger = ",// 22
-	};
+    public String[] person = {"How many persons will you type? ", // 0
+            "Datas of ", // 1
+            "a person: ", // 2
+            "Name: ", // 3
+            "Age: ", // 4
+            "Height: ", // 5
+            "Average height: ", // 6
+            "People whit less 16 year old: ", // 7
+            "Person: ", // 8
+            "A age avarege ", // 9
+            " is the ", // 10
+            " and ", // 11
+            " years old", // 12
+            " ", // 13
+            "Write ages: ", // 14
+            "How a many persons you go register? ", // 15
+            "Person more old: ", // 16
+            "Height of %da person: ", // 17
+            "Gender of %da person: ", // 18
+            "Number of mans = ", // 19
+            "Avarege height of womans = ", // 20
+            "Height smaller = ", // 21
+            "Height Bigger = ",// 22
+    };
 
-	public String[] product = { "Enter the product data:", // 0
-			"Product data: ", // 1
-			"Update data: ", // 2
-			"Enter the number of product to be added in stock: ", // 3
-			"Enter the number of product to be remove in stock: ", // 4
-			"Enter with unit price of product: ", // 5
-			"Enter the quantity purchased: ", // 6
-			"Money received: ", // 7
-			"Change: ", // 8
-			"Total value for pay: R$ ", // 9
-			"Enter the code of product acquired: ", // 10
-			"""
+    public String[] product = {"Enter the product data:", // 0
+            "Product data: ", // 1
+            "Update data: ", // 2
+            "Enter the number of product to be added in stock: ", // 3
+            "Enter the number of product to be remove in stock: ", // 4
+            "Enter with unit price of product: ", // 5
+            "Enter the quantity purchased: ", // 6
+            "Money received: ", // 7
+            "Change: ", // 8
+            "Total value for pay: R$ ", // 9
+            "Enter the code of product acquired: ", // 10
+            """
 					Products;
 					%s, which price is $ %.2f
 					%s, which price is $ %.2f
@@ -68,10 +73,10 @@ public class MensagensObjects {
 					Measue whit eight decimal places: %.8f
 					Rouded (three decimal places): %.3f
 					""", // 11
-			"Price of purchase: ", // 12
-			"Price of sale: ", // 13
-			"How a many data of products? ", "Product ", // 15
-			"""
+            "Price of purchase: ", // 12
+            "Price of sale: ", // 13
+            "How a many data of products? ", "Product ", // 15
+            """
 					Report
 					Again smaller of 10: %d
 					Again between 10 and 20: %d
@@ -81,24 +86,24 @@ public class MensagensObjects {
 					Total again: %.2f
 					""", // 16
 
-	};
+    };
 
-	public String[] student = { "Enter with the firsth note: ", // 0
-			"Enter with the second note: ", // 1
-			"Final note: ", // 2
-			"Reproved ", // 3
-			"Aproved", // 4
-			"Value invalid! try again: ", // 5
-			"Avarage = ", // 6
-			"Write the name, the firsth and second note of %d Student: ", // 7
-			"How a many students are registers? ", // 8
-			"Studenst aproved",// 9
-	};
+    public String[] student = {"Enter with the firsth note: ", // 0
+            "Enter with the second note: ", // 1
+            "Final note: ", // 2
+            "Reproved ", // 3
+            "Aproved", // 4
+            "Value invalid! try again: ", // 5
+            "Avarage = ", // 6
+            "Write the name, the firsth and second note of %d Student: ", // 7
+            "How a many students are registers? ", // 8
+            "Studenst aproved",// 9
+    };
 
-	public String[] pension = { "How many rooms will be rented? ", // 0
-			"Rent #%d: \n", // 1
-			"Email: ", // 2
-			"Room: ", // 3
-			"Busy rooms: ",// 4
-	};
+    public String[] pension = {"How many rooms will be rented? ", // 0
+            "Rent #%d: \n", // 1
+            "Email: ", // 2
+            "Room: ", // 3
+            "Busy rooms: ",// 4
+    };
 }
