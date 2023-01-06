f7f50475619a011dfbfa7e6853dd236dce4745fb
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-06 14:33:14 -0300

    Create list of orders per client e method getOrders

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,2 +1,50 @@
-package entities;public class Client {
+package entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    private List<Order> orders = new ArrayList<>();
+    public Client() {
+    }
+
+    public Client(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
