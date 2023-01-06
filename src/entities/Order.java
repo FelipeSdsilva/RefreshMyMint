bd844b8ca7b2bc47bf690d7998274dfce0d16dde
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-06 09:11:31 -0300

    Created class Order

---
+++ src/entities/Order.java
@@ -0,0 +1,54 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+    private Long id;
+    private LocalDateTime moment;
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(Long id, LocalDateTime moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDateTime moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
+}
