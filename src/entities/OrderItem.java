99f5df27e2901f2d60ab60bccf23d6b247d0eb42
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-06 14:02:16 -0300

    Fix the class OrderItem

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -1,2 +1,43 @@
-package entities;public class OrderItem {
+package entities;
+
+public class OrderItem {
+
+    private Integer qtd;
+    private Double price;
+
+   private  Product product;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Integer qtd, Double price, Product product) {
+        this.qtd = qtd;
+        this.price = price;
+        this.product = product;
+    }
+
+    public Integer getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(Integer qtd) {
+        this.qtd = qtd;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
 }
