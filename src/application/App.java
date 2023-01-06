91a227741c2523c9166dd532b6831f069e3318ab
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-06 14:54:36 -0300

    Initializing tests for exercice of Orders

--- src/application/App.java
+++ src/application/App.java
@@ -1,15 +1,24 @@
 package application;
 
-import controllers.InitialController;
-
 import java.util.Locale;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
+        Scanner in = new Scanner (System.in);
+        System.out.println("Welcome to store! ");
+        char cont ;
 
-        InitialController.menuInitial();
+        do{
+
+            System.out.print("Do you when continue? (y= yes/ n= no)");
+            cont = in.next().charAt(0);
+        }while (cont != 'n');
+
+        in.close();
+        //InitialController.menuInitial();
     }
 }
