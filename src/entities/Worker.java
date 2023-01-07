ccad793b1085e3855a653e224beabf9ee92e62fc
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-07 17:03:22 -0300

    Create class Worker in package entites

---
+++ src/entities/Worker.java
@@ -0,0 +1,71 @@
+package entities;
+
+import entities.enums.WorkLevel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Worker {
+
+    private String name;
+    private WorkLevel level;
+    private Double baseSalary;
+    private Department department;
+
+    List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {
+    }
+
+    public Worker(String name, WorkLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+}
